e534a81da9f78b162b3befb6386f4a2dfdb2f374
Ailton Nunes <ailton.souza@firstdecision.com.br>
2020-05-11 02:15:38 -0300

    Desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
@@ -11,7 +12,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (accum: Balance, curr: Transaction) => {
+        switch (curr.type) {
+          case 'income':
+            accum.income += Number(curr.value);
+            break;
+          case 'outcome':
+            accum.outcome += Number(curr.value);
+            break;
+          default:
+            break;
+        }
+
+        return accum;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import CategoryService from './CreateCategoryService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+// import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_title: string;
+}
+
+interface ReqCategory {
+  title: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_title,
+  }: Request): Promise<Transaction> {
+    const categoryService = new CategoryService();
+
+    const category = await categoryService.execute(category_title);
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome') {
+      const checkBalance = balance.income - (balance.outcome + value);
+
+      if (checkBalance < 0) {
+        throw new AppError('Value of outcome is insufficient');
+      }
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
